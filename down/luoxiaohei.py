4823919e09883aafa77f420516c343906819d2d8
lishulong <lishulong.never@gmail.com>
2019-09-27 19:12:26 +0800

    feat: 罗小黑战记

---
+++ down/luoxiaohei.py
@@ -0,0 +1,11 @@
+import requests
+
+if __name__ == '__main__':
+    with open('index.m3u8', 'r') as file:
+        fs = file.read().split('\n')
+    fs = [i for i in fs if not i.startswith('#')]
+    print(fs)
+    with open('luoxiaohei.mp4', 'wb') as file:
+        for i in fs:
+            rs = requests.get(f'https://baidu.com-ok-baidu.com/20190910/14935_da987396/1000k/hls/{i}')
+            file.write(rs.content)
